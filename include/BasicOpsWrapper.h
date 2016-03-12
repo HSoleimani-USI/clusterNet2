441ba97f098c7f696d229d0bfffc0b5fcc117e67
Tim Dettmers <tim.dettmers@gmail.com>
2016-03-12 12:19:21 +0100

    Re-enabled BasicOpsWrapper interface.

--- include/BasicOpsWrapper.h
+++ include/BasicOpsWrapper.h
@@ -17,7 +17,73 @@ public:
 	BasicOpsWrapper(){};
 	~BasicOpsWrapper(){};
 
+	virtual Matrix<float> *fill_matrix(int rows, int cols, float fill_value) = 0;
+	virtual Matrix<float> *empty(int rows, int cols) = 0;
+	virtual Matrix<float> *zeros(int rows, int cols) = 0;
+	virtual Matrix<float> *ones(int rows, int cols) = 0;
+	virtual void to_host(Matrix<float> *gpu, float *cpu) = 0;
+	virtual void to_gpu(float *cpu, Matrix<float> *gpu) = 0;
+	virtual Matrix<float> *to_pinned(int rows, int cols, float *cpu) = 0;
+	virtual Matrix<float> *to_pinned(int rows, int cols, float *cpu, size_t bytes_to_copy) = 0;
 
+
+	virtual void sortbykey(Matrix<float> *keys, Matrix<float> *values) = 0;
+	virtual float sum(Matrix<float> *A) = 0;
+
+	virtual void transpose(Matrix<float> *A, Matrix<float> *out, int rows, int cols) = 0;
+	virtual Matrix<float> *to_col_major(Matrix<float> *A) = 0;
+	virtual void to_col_major(Matrix<float> *A, Matrix<float> *out) = 0;
+	virtual Matrix<float> *to_row_major(Matrix<float> *A) = 0;
+	virtual Matrix<float> *transpose(Matrix<float> *A) = 0;
+
+	/*
+	template <int action> void elementWise(Matrix<float> *A, Matrix<float>*out) = 0;
+	template <int action> void elementWise(Matrix<float> *A, Matrix<float>*out, float scalar) = 0;
+	template <int action> void elementWise(Matrix<float> *A, Matrix<float> *B, Matrix<float>*out) = 0;
+	template <int action> void elementWise(Matrix<float> *A, Matrix<float> *B, Matrix<float>*out, float scalar) = 0;
+
+	template <int action> void vectorWise(Matrix<float> *v, Matrix<float>*out) = 0;
+	template <int action> void vectorWise(Matrix<float> *A, Matrix<float> *v, Matrix<float>*out) = 0;
+	*/
+
+
+	virtual void slice(Matrix<float> *A, Matrix<float>*out, int rstart, int rend, int cstart, int cend) = 0;
+	virtual void softmax(Matrix<float> *A, Matrix<float> *out) = 0;
+	virtual void argmax(Matrix<float> *A, Matrix<float> *out) = 0;
+
+
+	virtual void lookup(Matrix<float> *embedding, Matrix<float> *idx_batch, Matrix<float> *out) = 0;
+	virtual void embeddingUpdate(Matrix<float> *embedding, Matrix<float> *idx_batch, Matrix<float> *grad, Matrix<float> *RMS, float RMS_momentum, float learning_rate) = 0;
+
+	/*
+	template <int action> void WeightUpdate(Matrix<float> *RMS, Matrix<float> *grad, Matrix<float> *w, float RMS_multiplier, float learning_rate) = 0;
+
+	template <int reduction> void reduceToCols(Matrix<float> *A, Matrix<float> *vout) = 0;
+	template <int reduction> void reduceToRows(Matrix<float> *A, Matrix<float> *vout) = 0;
+	template <int reduction> float reduceToValue(Matrix<float> *A) = 0;
+	template <int reduction> float reduceToValue(Matrix<float> *A, Matrix<float> *vout_rows) = 0;
+	*/
+
+
+	virtual bool check_matrix_vector_op(Matrix<float> *A, Matrix<float> *vec) = 0;
+	virtual bool check_for_same_dimensions(Matrix<float> *A, Matrix<float> *B) = 0;
+	virtual bool check_matrix_multiplication(Matrix<float> *A, Matrix<float> *B, Matrix<float> *out, bool T1, bool T2) = 0;
+
+
+	virtual Matrix<float> *read_hdf5(const char *filepath) = 0;
+	virtual Matrix<float> *read_hdf5(const char *filepath, const char *tag) = 0;
+
+	virtual Matrix<float> *get_view(Matrix<float> *A, int rstart, int rend) = 0;
+
+
+	virtual void print_matrix(Matrix<float> *A, int end_rows, int end_cols) = 0;
+	virtual void print_matrix(Matrix<float> *A, int start_row, int end_row, int start_col, int end_col) = 0;
+	virtual void printmat(Matrix<float> *A) = 0;
+	virtual void printhostmat(Matrix<float> *A) = 0;
+	virtual void printdim(Matrix<float> *A) = 0;
+	virtual void printsum(Matrix<float> *A) = 0;
+	virtual void printmat(Matrix<float> *A, int end_rows, int end_cols) = 0;
+	virtual void printmat(Matrix<float> *A, int start_row, int end_row, int start_col, int end_col) = 0;
 
 };
 
