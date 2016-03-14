c8b2081e29cd790db53f77360b28ae2fc2d21991
Tim Dettmers <tim.dettmers@gmail.com>
2016-03-14 14:03:41 +0100

    added CPU function for fill_matrix

--- cpp_source/BasicOpsWrapperCPU.cpp
+++ cpp_source/BasicOpsWrapperCPU.cpp
@@ -7,3 +7,20 @@
 
 #include "BasicOpsWrapperCPU.h"
 
+
+
+
+Matrix<float> *fill_matrix(int rows, int cols, float fill_value)
+{
+	Matrix<float> *ret = new Matrix<float>();
+	ret->data = (float*)malloc(sizeof(float)*rows*cols);
+	ret->rows = rows;
+	ret->cols = cols;
+	ret->size = rows*cols;
+	ret->bytes = rows*cols*size(float);
+
+	for(int i = 0; i < ret->size; i++)
+		ret->data[i] = fill_value;
+
+	return ret;
+}
